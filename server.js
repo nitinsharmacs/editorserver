05fddc673758f9c8cc9a07ab33bc0e82d5d0c7be
nitinsharmacs <ramujha266@gmail.com>
2020-07-02 15:04:33 +0530

    errof 6

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const lang = {
 server.use((req, res, next)=>{
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PETCH, DELETE, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	if(req.method == 'OPTIONS'){
 		return res.sendStatus(200);
 	}
