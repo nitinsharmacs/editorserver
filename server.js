f06890304c463c91f2e3be6660aabcba3d10960f
nitinsharmacs <ramujha266@gmail.com>
2020-07-02 14:26:15 +0530

    deploying problem 1

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 //const compiler = require('compilex');
-const compilers = require('./compilers/compilers');
+//const compilers = require('./compilers/compilers');
 const server = express();
 
-const option = {init:true};
-compilers.init(option);
+// const option = {init:true};
+// compilers.init(option);
 
-const lang = {
-	cpp:{compiler:compilers.compileCPP},
-	python:{compiler:compilers.compilePython}
-};
+// const lang = {
+// 	cpp:{compiler:compilers.compileCPP},
+// 	python:{compiler:compilers.compilePython}
+// };
 
 server.use(bodyParser.json());
 
@@ -26,28 +26,28 @@ server.use((req, res, next)=>{
 });
 
 
-server.post('/compile/:lang', (req, res, next)=> {
-	console.log(req.params)
-	if(!req.body.input && !req.body.code){
-		return res.status(400).json({result:'No Code, No Input thus No response ...'})
-	}
-	if(req.params.lang !== 'cpp' && req.params.lang !== 'python'){
-		return res.status(400).json({result:'Non supported language'});
-	}
-	const envData = {
-		OS:process.env.OS,cmd:process.env.CMD
-	}
-	lang[req.params.lang].compiler(envData,req.body.code,req.body.input, data=>{
-		res.status(201).json({result:data.output})
-		if(data.filepath)
-			fs.unlinkSync(data.filepath);
-		if(data.objectCodeFilePath)
-			fs.unlinkSync(data.objectCodeFilePath)
-		if(data.inputFilePath)
-			fs.unlinkSync(data.inputFilePath);
-	});
+// server.post('/compile/:lang', (req, res, next)=> {
+// 	console.log(req.params)
+// 	if(!req.body.input && !req.body.code){
+// 		return res.status(400).json({result:'No Code, No Input thus No response ...'})
+// 	}
+// 	if(req.params.lang !== 'cpp' && req.params.lang !== 'python'){
+// 		return res.status(400).json({result:'Non supported language'});
+// 	}
+// 	const envData = {
+// 		OS:process.env.OS,cmd:process.env.CMD
+// 	}
+// 	lang[req.params.lang].compiler(envData,req.body.code,req.body.input, data=>{
+// 		res.status(201).json({result:data.output})
+// 		if(data.filepath)
+// 			fs.unlinkSync(data.filepath);
+// 		if(data.objectCodeFilePath)
+// 			fs.unlinkSync(data.objectCodeFilePath)
+// 		if(data.inputFilePath)
+// 			fs.unlinkSync(data.inputFilePath);
+// 	});
 		
-});
+// });
 server.use((req, res)=>{
 	res.send('server is running')
 })
