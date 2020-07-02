ed956a786562ace4405ce0c38d624a367a0b8172
nitinsharmacs <ramujha266@gmail.com>
2020-07-02 15:25:28 +0530

    error

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ const lang = {
 
 
 server.use((req, res, next)=>{
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PETCH, DELETE, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    
 	if(req.method == 'OPTIONS'){
 		return res.sendStatus(200);
 	}
