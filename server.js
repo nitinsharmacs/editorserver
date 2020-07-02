d634f55237aba169495a2ce8151784634db9d206
nitinsharmacs <ramujha266@gmail.com>
2020-07-02 15:29:22 +0530

    error

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const lang = {
 
 
 server.use((req, res, next)=>{
-	res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     
 	if(req.method == 'OPTIONS'){
 		return res.sendStatus(200);
